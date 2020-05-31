036ba8d94c73eb66f592493c046bd8f1942bccc3
Shangxin <shangxin@outlook.com>
2020-05-31 10:52:55 +0800

    feat: create Track and BaseItem componets

---
+++ src/components/editor/Track/BaseItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './style/track.module.css'
+
+export default class BaseItem extends React.Component {
+    render () {
+        return (
+            <>
+            </>
+        )
+    }
+}
---
+++ src/components/editor/Track/Track.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './style/track.module.css'
+
+export default class Track extends React.Component {
+    render () {
+        return (
+            <>
+            </>
+        )
+    }
+}
