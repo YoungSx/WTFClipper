ece2f71a2c9867915b6aa7cd2a1e959f939f9106
Shangxin <shangxin@outlook.com>
2020-07-07 02:33:12 +0800

    feat: item trimmer move

--- src/components/editor/Track/BaseItem.tsx
+++ src/components/editor/Track/BaseItem.tsx
@@ -24,8 +24,16 @@ interface BaseItemState {
         left: number,
         top: number
     },
+    trimmersMoveStartStatus: {
+        x: number,
+        y: number,
+        left: number,
+        width: number,
+        offset: number
+    },
     itemEle: HTMLElement | null,
-    makersEle: HTMLElement | null
+    makersEle: HTMLElement | null,
+    offset: number
 }
 
 export default class BaseItem extends React.Component<BaseItemProps, BaseItemState> {
@@ -47,8 +55,16 @@ export default class BaseItem extends React.Component<BaseItemProps, BaseItemSta
                 left: 0,
                 top: 0
             },
+            trimmersMoveStartStatus: {
+                x: 0,
+                y: 0,
+                left: 0,
+                width: 0,
+                offset: 0
+            },
             itemEle: null,
-            makersEle: null
+            makersEle: null,
+            offset: 0
         }
     }
 
@@ -61,6 +77,7 @@ export default class BaseItem extends React.Component<BaseItemProps, BaseItemSta
             makersEle: document.getElementById('makers_area')
         }, () => {
             this.bindItemMoveEvent(this.state.itemEle, this.state.makersEle)
+            this.bindTrimmerMoveEvent(this.state.itemEle, this.state.makersEle)
         })
     }
 
@@ -98,10 +115,78 @@ export default class BaseItem extends React.Component<BaseItemProps, BaseItemSta
         makersEle?.addEventListener('mouseup', itemMouseUp)
     }
 
+    bindTrimmerMoveEvent (itemEle: HTMLElement | null, makersEle: HTMLElement | null) {
+        const leftTrimmer = document.getElementById(this.state.innerTrackItemTrimmerLeftId)
+        const rightTrimmer = document.getElementById(this.state.innerTrackItemTrimmerRightId)
+        // left trimmer events
+        const leftTrimmerMouseDown = (e: any) => {
+            e.stopPropagation()
+            this.storeTrimmersMoveStartStatus(e, itemEle)
+            leftTrimmer?.addEventListener('mousemove', leftTrimmerMouseMove)
+            makersEle?.addEventListener('mousemove', leftTrimmerMouseMove)
+        }
+        const leftTrimmerMouseMove = (e: any) => {
+            this.TrimmerMove(e, 'left', leftTrimmer, itemEle)
+        }
+        const leftTrimmerMouseUp = (e: any) => {
+            leftTrimmer?.removeEventListener('mousemove', leftTrimmerMouseMove)
+            makersEle?.removeEventListener('mousemove', leftTrimmerMouseMove)
+        }
+        leftTrimmer?.addEventListener('mousedown', leftTrimmerMouseDown)
+        leftTrimmer?.addEventListener('mouseup', leftTrimmerMouseUp)
+        makersEle?.addEventListener('mouseup', leftTrimmerMouseUp)
+        //right trimmer events
+        const rightTrimmerMouseDown = (e: any) => {
+            e.stopPropagation()
+            this.storeTrimmersMoveStartStatus(e, itemEle)
+            rightTrimmer?.addEventListener('mousemove', rightTrimmerMouseMove)
+            makersEle?.addEventListener('mousemove', rightTrimmerMouseMove)
+        }
+        const rightTrimmerMouseMove = (e: any) => {
+            this.TrimmerMove(e, 'right', rightTrimmer, itemEle)
+        }
+        const rightTrimmerMouseUp = (e: any) => {
+            rightTrimmer?.removeEventListener('mousemove', rightTrimmerMouseMove)
+            makersEle?.removeEventListener('mousemove', rightTrimmerMouseMove)
+        }
+        rightTrimmer?.addEventListener('mousedown', rightTrimmerMouseDown)
+        rightTrimmer?.addEventListener('mouseup', rightTrimmerMouseUp)
+        makersEle?.addEventListener('mouseup', rightTrimmerMouseUp)
+    }
+
     itemMove (e: any) {
         // 鼠标 x 轴偏移量 + 起始 left
         let left = e.clientX - this.state.itemMoveStartStatus.x + this.state.itemMoveStartStatus.left
         this.setItemLeft(left)
+    }
+
+    TrimmerMove (e: any, orient: string, trimmer: any, itemEle: any) {
+        if (orient === 'left') {
+            let x = e.clientX - this.state.trimmersMoveStartStatus.x
+                + this.state.trimmersMoveStartStatus.left
+                - itemEle.parentNode.getBoundingClientRect().left
+            let newWidth = this.state.trimmersMoveStartStatus.width
+                - (e.clientX - this.state.trimmersMoveStartStatus.x
+                - itemEle.parentNode.getBoundingClientRect().left)
+            this.setItemLeft(x, () => {
+                this.setItemWidth(newWidth)
+            })
+        } else {
+            let x = e.clientX - itemEle.parentNode.getBoundingClientRect().left
+            let left = itemEle.getBoundingClientRect().left - itemEle.parentNode.getBoundingClientRect().left
+            /*
+                计算说明:
+                width = 当前鼠标坐标 - item 左边距 + 鼠标坐标距离 item 最右边距离
+                        = 当前鼠标坐标 - (item 屏幕左边距 - item 父元素屏幕左边距) + (鼠标刚点下时的宽度 - 鼠标刚点下时鼠标坐标距离 item 最右边距离)
+                        = 当前鼠标坐标 - (item 屏幕左边距 - item 父元素屏幕左边距) + (鼠标刚点下时的宽度 - (鼠标刚点下时坐标 - 鼠标刚点下时 item 左边距))
+            */
+            let newWidth = x - (itemEle.getBoundingClientRect().left
+                - itemEle.parentNode.getBoundingClientRect().left)
+                + this.state.trimmersMoveStartStatus.width
+                - (this.state.trimmersMoveStartStatus.x
+                - this.state.trimmersMoveStartStatus.left)
+            this.setItemWidth(newWidth)
+        }
     }
 
     storeItemMoveStartStatus (e: any, itemEle: any) {
@@ -111,6 +196,18 @@ export default class BaseItem extends React.Component<BaseItemProps, BaseItemSta
                 y: e.clientY,
                 left: itemEle.getBoundingClientRect().left - itemEle.parentNode.getBoundingClientRect().left,
                 top: itemEle.getBoundingClientRect().top - itemEle.parentNode.getBoundingClientRect().top
+            }
+        })
+    }
+
+    storeTrimmersMoveStartStatus (e: any, itemEle: any) {
+        this.setState({
+            trimmersMoveStartStatus: {
+                x: e.clientX - itemEle.parentNode.getBoundingClientRect().left,
+                y: 0,
+                left: itemEle.getBoundingClientRect().left - itemEle.parentNode.getBoundingClientRect().left,
+                width: itemEle.getBoundingClientRect().width,
+                offset: this.state.offset
             }
         })
     }
