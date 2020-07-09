6a778a2bb0321c3d642eb545c20195cd509b3818
Shangxin <shangxin@outlook.com>
2020-07-10 02:44:40 +0800

    feat: my medias resource tab

--- src/components/editor/ResourceLibrary.tsx
+++ src/components/editor/ResourceLibrary.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Tabs } from 'antd'
+
+const { TabPane } = Tabs
 
 export default class Makers extends React.Component {
     render () {
         return (
             <>
-                Resource Library
+                <Tabs defaultActiveKey="1" tabPosition="left" style={{ height: 220 }}>
+                    <TabPane tab="My medias" key="1">
+                        there are some medias
+                    </TabPane>
+                </Tabs>
             </>
         )
     }
