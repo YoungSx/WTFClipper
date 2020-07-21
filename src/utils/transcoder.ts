fcd15836ac7e61d82e8efc15f3ec269548718a65
Shangxin <shangxin@outlook.com>
2020-07-22 00:47:28 +0800

    feat: add Tanscoder class

--- src/utils/transcoder.ts
+++ src/utils/transcoder.ts
@@ -1,4 +1,22 @@
+import { appData } from '../config'
+import { parseFileNameFromPath } from '../utils/file'
+
+const child_process = require('child_process')
+
 class Transcoder {
+    generateCoverImage (input: string, callback?: any) {
+        let outputPath = `${appData.COVER_DIR}/${parseFileNameFromPath(input).nameWithoutExt}.${appData.COVER_EXT}`
+        let cmd = `ffmpeg -i ${input} -ss 1.0 -vframes 1 -vf scale=480:270 ${outputPath} -y`
+        let result = child_process.exec(cmd, function(err: string, stdout: string, stderr: string){
+            if (err) {
+                callback(err)
+            } else if (stdout) {
+                callback(stdout)
+            } else {
+                callback(stderr)
+            }
+        })
+    }
 }
 
 export default Transcoder
