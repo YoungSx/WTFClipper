e2d8be65a115d100c5b4baf2d41758695cb2a956
Shangxin <shangxin@outlook.com>
2020-07-23 02:25:57 +0800

    fix: fix parseFileNameFromPath

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,10 +1,12 @@
 export function parseFileNameFromPath (path:string) {
-    let name = path.substring(path.lastIndexOf('/') + 1, path.length)
+    let tempIndex1 = path.lastIndexOf('/')
+    let tempIndex2 = path.lastIndexOf('\\')
+    let name = path.substring((tempIndex1 > tempIndex2 ? tempIndex1 : tempIndex2) + 1, path.length)
     let tempArr = name.split('.')
     let nameWithoutExt = tempArr[0]
     return {
         name,
         nameWithoutExt,
-        ext: tempArr
+        ext: tempArr.length > 1 ? tempArr[1] : ''
     }
 }
