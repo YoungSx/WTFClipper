b1d46d1bbb5cc7971eed40c73aebbcc2ce884b18
Shangxin <shangxin@outlook.com>
2020-07-30 02:24:16 +0800

    feat: add PRIVATE_DIR to appdata config

--- src/config/appdata.ts
+++ src/config/appdata.ts
@@ -1,6 +1,13 @@
 var appData = {
+    /**
+     * TODO:
+     *  1. 检测目录是否存在，不存在则创建
+     */
     get RESOURCE_HOME () {
         return 'C:/Users/shangxin/Videos/WTFClipper'
+    },
+    get PRIVATE_DIR () {
+        return this.RESOURCE_HOME + '/private'
     },
     get CACHE_DIR () {
         return this.RESOURCE_HOME + '/cache'
