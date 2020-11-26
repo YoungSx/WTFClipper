15976ed74705b580a4621d27b546991a758eb322
Shangxin <shangxin@outlook.com>
2020-11-27 02:28:37 +0800

    feat: add UUID generator

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -22,3 +22,16 @@ export function getCoverImage (src: string, callback: any) {
         callback(result)
     })
 }
+
+export function UUID() {
+    let str = '0123456789abcdef'
+    let arr = []
+    for(let i = 0; i < 36; i++){
+      arr.push(str.substr(Math.floor(Math.random() * 0x10), 1))
+    }
+    arr[14] = 4;
+    arr[19] = str.substr(<number>arr[19] & 0x3 | 0x8, 1)
+    arr[8] = arr[13] = arr[18] = arr[23] = '-'
+    return arr.join('')
+}
+  
