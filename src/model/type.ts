b84b90d32b8d48778b05dbc4ae1694d7a4796a86
Shangxin <shangxin@outlook.com>
2020-11-28 02:55:15 +0800

    feat: add file type model

---
+++ src/model/type.ts
@@ -0,0 +1,22 @@
+export interface BaseFileType {
+    id: string,
+    name: string,
+    displayName: string,
+    type: string
+}
+
+export interface MediaFileType extends BaseFileType {
+    path: string
+}
+
+export interface VideoFileType extends MediaFileType {
+    type: 'video'
+}
+
+export interface AudioFileType extends MediaFileType {
+    type: 'audio'
+}
+
+export interface PictureFileType extends MediaFileType {
+    type: 'picture'
+}
