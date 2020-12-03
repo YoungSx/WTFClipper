4265a39d17af6aa038516ca817ea82027e0d84d5
Shangxin <shangxin@outlook.com>
2020-12-04 01:43:55 +0800

    feat: add deepCopy to tools

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -6,3 +6,27 @@ export function inArray (search: any, array: any) {
     }
     return false
 }
+
+export function deepCopy(source: any): any {
+    if(null == source || {} == source || [] == source) return source
+    let newObject : any
+    let isArray = false
+    if((source as any).length) {
+        newObject = []
+        isArray = true
+    } else {
+        newObject = {}
+        isArray = false
+    }
+    for (let key of Object.keys(source)) {
+        if(null == source[key]) {
+            if (isArray) newObject.push(null)
+            else newObject[key] = null
+        } else {
+            let sub = (typeof source[key] == 'object') ? deepCopy(source[key]) : source[key]
+            if(isArray) newObject.push(sub)
+            else newObject[key] = sub
+        }
+    }
+    return newObject
+}
