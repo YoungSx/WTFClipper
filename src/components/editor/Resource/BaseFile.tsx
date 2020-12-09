122e8ec190f420f87792ebf1be5aff00255a7d78
Shangxin <shangxin@outlook.com>
2020-12-09 23:21:29 +0800

    feat: [BaseFile] support add file to Makers/Track

--- src/components/editor/Resource/BaseFile.tsx
+++ src/components/editor/Resource/BaseFile.tsx
@@ -7,13 +7,43 @@ interface FileProps {
     file: BaseFileType
 }
 
-export default class BaseFile extends React.Component<FileProps> {
+interface FileState {
+    rid: string,
+    eleId: string,
+    fileEle: any
+}
+
+export default class BaseFile extends React.Component<FileProps, FileState> {
+    constructor (props: FileProps) {
+        super(props)
+        this.state = {
+            rid: props.file.id,
+            eleId: 'r-' + props.file.id,
+            fileEle: null
+        }
+    }
+
+    componentDidMount () {
+        this.setState({
+            fileEle: document.getElementById(this.state.eleId)
+        }, () => {
+            this.bindFileDragEvent(this.state.fileEle)
+        })
+    }
+
+    bindFileDragEvent (fileEle: any) {
+        const fileDragStart = (e: any) => {
+            e.dataTransfer.setData("rid", this.state.rid)
+        }
+        fileEle?.addEventListener("dragstart", fileDragStart)
+    }
+
     render () {
         return (
             <>
-                <div className={ style.file } title={ this.props.file.displayName }>
-                    <FileUnknownTwoTone style={{ fontSize: '80px' }} />
-                    <div className={ style.file_name }>{ this.props.file.displayName }</div>
+                <div id={ this.state.eleId } className={ style.file } title={ this.props.file.displayName } draggable="true">
+                    <FileUnknownTwoTone style={{ fontSize: '80px' }} draggable="false"/>
+                    <div className={ style.file_name } draggable="false">{ this.props.file.displayName }</div>
                 </div>
             </>
         )
