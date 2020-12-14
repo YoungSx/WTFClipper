d513a9f9e7ceea796621487a3d522c4426c51bea
Shangxin <shangxin@outlook.com>
2020-12-14 23:15:19 +0800

    feat: tracks area scroll

--- src/components/editor/Makers.tsx
+++ src/components/editor/Makers.tsx
@@ -10,6 +10,51 @@ import { MakersStoreModel } from '../../model/type'
 import store from '../../redux'
 
 class Makers extends React.Component<MakersStoreModel> {
+interface MakersState {
+    tracksAreaEle: any,
+    header: {
+        offset: number,
+        width: number
+    }
+}
+
+class Makers extends React.Component<MakersStoreModel, MakersState> {
+    constructor (props: MakersStoreModel) {
+        super(props)
+        this.state = {
+            tracksAreaEle: null,
+            header: {
+                offset: 0,
+                width: 0
+            }
+        }
+    }
+
+    componentDidMount () {
+        this.setState({
+            tracksAreaEle: document.getElementById('tracks_area')
+        }, () => {
+            // init Element position info
+            this.tracksAreaScroll()
+            this.bindMakersEvent()
+        })
+    }
+
+    bindMakersEvent () {
+        const tracksAreaScroll = (e: any) => {
+            this.tracksAreaScroll()
+        }
+        this.state.tracksAreaEle.addEventListener('scroll', tracksAreaScroll)
+    }
+    tracksAreaScroll () {
+        this.setState({
+            header: {
+                offset: this.state.tracksAreaEle.scrollLeft,
+                width: this.state.tracksAreaEle.getBoundingClientRect().width
+            }
+        })
+    }
+
     render () {
         const trackItems = this.props['tracks'].map((track) => {
             return <Track className={style.track} key={`track_${track.id}`} track={track}></Track>
@@ -19,6 +64,7 @@ class Makers extends React.Component<MakersStoreModel> {
                 <MakersToolbar className={style.makers_toolbar}></MakersToolbar>
                 <Timeline className={style.timeline}></Timeline>
                 <div className={style.tracks_area}>
+                <div id="tracks_area" className={style.tracks_area}>
                     { trackItems }
                 </div>
             </>
