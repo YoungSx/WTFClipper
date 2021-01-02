39da9867bbb51b716d7d04612514e89088c8131a
Shangxin <shangxin@outlook.com>
2021-01-02 23:23:22 +0800

    feat: output button

--- src/components/editor/MakersToolbar.tsx
+++ src/components/editor/MakersToolbar.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React from 'react'
+import { Button, Divider } from 'antd'
+
+import Transcoder from '../../utils/transcoder'
+
+import store from '../../redux'
 
 interface MakersToolbarProps {
     className: string
 }
 
 export default class MakersToolbar extends React.Component<MakersToolbarProps> {
+    output () {
+        let descriptor = store.getState().makers.tracks
+        if (descriptor.length > 0) {
+            const transcoder = new Transcoder()
+            transcoder.produce({
+                id: 'h7h8b8g8g8g8',
+                createTime: +new Date(),
+                updateTime: +new Date(),
+                tracks: descriptor
+            })
+        }
+    }
+
     render () {
         return (
             <>
-                <div className={this.props.className}>Toolbar</div>
+                <div className={this.props.className}>
+                    <Button type="primary"
+                        style={{ height: '32px', margin: '4px', float: 'right' }}
+                        onClick={() => { this.output() }}>
+                        Output
+                    </Button>
+                </div>
             </>
         )
     }
